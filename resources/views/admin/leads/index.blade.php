06ae2be24995f0a6332bbdbae33ddf424c06e896
artic0909 <dev.saklin@gmail.com>
2026-08-06 16:47:53 +0530

    dasdasd

--- resources/views/admin/leads/index.blade.php
+++ resources/views/admin/leads/index.blade.php
@@ -668,8 +668,10 @@
                                         <a href="{{ route($routePrefix . '.meetings.create', ['lead_id' => $lead->id]) }}" class="ra-btn" title="Meeting"><i class="bi bi-camera-video-fill"></i></a>
 
                                         <a href="{{ route($routePrefix . '.leads.show', $lead->id) }}" class="ra-btn" title="View"><i class="bi bi-eye-fill"></i></a>
+                                        @if(!($routePrefix == 'sale' && request('type') == 'total'))
                                         <a href="{{ route($routePrefix . '.leads.followup', ['id' => $lead->id, 'return_url' => request()->fullUrl()]) }}" class="ra-btn" title="Followup"><i class="bi bi-arrow-counterclockwise"></i></a>
                                         <a class="ra-btn" title="Edit" href="{{ route($routePrefix . '.leads.edit', $lead->id) }}"><i class="bi bi-pencil-fill"></i></a>
+                                        @endif
                                       
                                         @if($routePrefix == 'admin')
                                         <button class="ra-btn danger" title="Delete" onclick="confirmDelete('{{ route($routePrefix . '.leads.destroy', $lead->id) }}')"><i class="bi bi-trash-fill"></i></button>
