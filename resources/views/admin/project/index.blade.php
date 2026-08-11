97708bad72904ef19faea0756680f44a21cb695c
artic0909 <dev.saklin@gmail.com>
2026-08-11 09:46:10 +0530

    country code

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -261,7 +261,14 @@
                                     <div class="ln">{{ $project->client_name }}</div>
                                     <div class="ls">
                                         @if(is_array($project->phones) && count($project->phones) > 0)
-                                            {{ is_array($project->phones[0]) ? ($project->phones[0]['number'] ?? $project->phones[0]['num'] ?? 'N/A') : $project->phones[0] }}
+                                            @php
+                                                $firstPhone = $project->phones[0];
+                                                $codes = [0=>'+93',1=>'+355',2=>'+213',3=>'+376',4=>'+244',5=>'+54',6=>'+61',7=>'+43',8=>'+880',9=>'+32',10=>'+55',11=>'+1',12=>'+86',13=>'+57',14=>'+45',15=>'+20',16=>'+33',17=>'+49',18=>'+233',19=>'+30',20=>'+91',21=>'+62',22=>'+98',23=>'+964',24=>'+353',25=>'+972',26=>'+39',27=>'+81',28=>'+962',29=>'+254',30=>'+965',31=>'+961',32=>'+60',33=>'+52',34=>'+212',35=>'+977',36=>'+31',37=>'+64',38=>'+234',39=>'+47',40=>'+968',41=>'+92',42=>'+63',43=>'+48',44=>'+351',45=>'+974',46=>'+7',47=>'+966',48=>'+65',49=>'+27',50=>'+34',51=>'+94',52=>'+46',53=>'+41',54=>'+886',55=>'+66',56=>'+90',57=>'+971',58=>'+44',59=>'+1',60=>'+84',61=>'+260',62=>'+263'];
+                                                $num = is_array($firstPhone) ? ($firstPhone['number'] ?? $firstPhone['num'] ?? 'N/A') : $firstPhone;
+                                                $phoneCode = (is_array($firstPhone) && isset($firstPhone['code_idx']) && isset($codes[$firstPhone['code_idx']])) ? $codes[$firstPhone['code_idx']] : '';
+                                                $displayPhone = $phoneCode && $num != 'N/A' ? $phoneCode . ' ' . $num : $num;
+                                            @endphp
+                                            {{ $displayPhone }}
                                             @if(count($project->phones) > 1) <small class="text-muted">(+{{ count($project->phones)-1 }})</small> @endif
                                         @elseif($project->phones)
                                             {{ $project->phones }}
